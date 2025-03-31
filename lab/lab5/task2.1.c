44ed3e6397f2f79ea352c9434eec0b27800bd825
HiIAmTzeKean <ngtzekean@gmail.com>
2025-03-31 17:53:51 +0800

    fix overlapping cuda code

--- lab/lab5/task2.1.c
+++ lab/lab5/task2.1.c
@@ -1,11 +1,12 @@
-__global__ void cuda_hello(){
+__global__ void cuda_hello(int n) {
     printf("Hello from GPU%d[%d]!\n",
-           blockIdx.x, threadIdx.x);
+           n, threadIdx.x);
 }
 
 int main() {
     printf("Hello from CPU!\n");
-    cuda_hello<<<1,4>>>();
-    cuda_hello<<<1,6>>>();
+    cuda_hello<<<1,4>>>(1);
+    cudaDeviceSynchronize();
+    cuda_hello<<<1,6>>>(2);
     return 0;
 }
