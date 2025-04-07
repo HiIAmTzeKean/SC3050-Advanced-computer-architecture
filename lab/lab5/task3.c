30258940169e75e83ab146e57902fee22b153add
HiIAmTzeKean <ngtzekean@gmail.com>
2025-04-07 16:42:52 +0800

    rename file

--- lab/lab5/task3.c
+++ lab/lab5/task3.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 #include <cuda_runtime.h>
 
-__global__ void vector_dot_product(int *day, float *price, float *c) {
+__global__ void calculator(int *day, float *price, float *c) {
     __shared__ float temp[7];
     __shared__ float price_temp[4];
-    // Copy the price array to shared memory
     if (threadIdx.x < 4) {
         price_temp[threadIdx.x] = price[threadIdx.x];
     }
-    __syncthreads(); // Synchronize threads within a block
+    __syncthreads();
 
     int i = blockIdx.x * blockDim.x + threadIdx.x;
     for (int j = 0; j < 4; j++) {
-        temp[i] += day[j][i] * price_temp[j];
+        temp[i] += day[j*7 + i] * price_temp[j];
     }
+    c[i] = temp[i];
 }
 int main() {
     int day[4][7] = {
@@ -24,23 +24,22 @@ int main() {
     };
     float price[4] = {29.99, 14.99, 9.99, 24.99};
     float c[7];
-    float *d_day, *d_price, *d_c;
+    int *d_day;
+    float *d_price, *d_c;
     // Allocate memory on the GPU
     cudaMalloc((void**)&d_day, 7 * 4 * sizeof(int));
     cudaMalloc((void**)&d_price, 4 * sizeof(float));
     cudaMalloc((void**)&d_c, 7 * sizeof(float));
-    // Copy the input vectors from host memory to GPU buffers
-    cudaMemcpy(d_day, day, size, cudaMemcpyHostToDevice);
-    cudaMemcpy(d_price, price, size, cudaMemcpyHostToDevice);
-    // Launch the vector_add kernel on GPU
+    cudaMemcpy(d_day, day, 7 * 4 * sizeof(int), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_price, price, 4 * sizeof(float), cudaMemcpyHostToDevice);
     vector_dot_product<<<1, 7>>>(d_day, d_price, d_c);
-    // Copy the result vector from GPU buffer to host memory
-    cudaMemcpy(c, d_c, sizeof(float), cudaMemcpyDeviceToHost);
-    // Free GPU memory
+    cudaDeviceSynchronize();
+
+    cudaMemcpy(c, d_c, 7 * sizeof(float), cudaMemcpyDeviceToHost);
     cudaFree(d_day);
     cudaFree(d_price);
     cudaFree(d_c);
-    // Print the result
+    
     for (int i = 0; i < 7; i++) {
         printf("Sales for [%d]: %f\n", i, c[i]);
     }
